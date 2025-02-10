a801253f186029d5019da8efdf46097010c62f9d
Joel <joel.fagersand@gmail.com>
2025-02-10 11:20:33 +0100

    finish assignment
    Co-authored-by: Olleand05 <Olleand05@users.noreply.github.com>
    Co-authored-by: HugoLennartsson <HugoLennartsson@users.noreply.github.com>

--- labfile.py
+++ labfile.py
@@ -1,11 +1,30 @@
 import numpy 
+import math
+
+def projection(v1, v2):
+    factor = (numpy.dot(v1,v2))/numpy.dot(v2,v2)
+    return [factor*v for v in v2]
+
+def sub(v1, v2):
+    i = 0
+    sumvector = []
+    while i < len(v1):
+        sumvector.append(v1[i] - v2[i])
+        i += 1
+    return sumvector
 
 def calculate_projection_normalvector(point, normalvector):
-    return numpy.dot(point, normalvector)
+    xn = projection(point, normalvector)
+    return sub(point,xn)
 
 def calculate_projection_angular_vectors(point, u, v):
     normalvector = numpy.cross(u,v)
     return calculate_projection_normalvector(point, normalvector)
 
 print(calculate_projection_angular_vectors([numpy.pi, numpy.e, 1], [1,1,0], [0,-1,0]))
+print(calculate_projection_angular_vectors([numpy.pi, numpy.e, 1], [1,1,0], [0,1,0]))
+print(calculate_projection_normalvector([numpy.pi, numpy.e, 1], [1,1,1]))
 print(calculate_projection_normalvector([numpy.pi, numpy.e, 1], [3,3,3]))
+
+
+# se bild för motivering
